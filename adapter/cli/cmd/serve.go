83a2b3e3a82c0ff3094f3d208b169cedee8678f3
Vincius Boscardin <boscardinvinicius@gmail.com>
2022-07-24 14:33:36 -0300

    :sparkles: configura endpoints da aplicação

--- adapter/cli/cmd/serve.go
+++ adapter/cli/cmd/serve.go
@@ -5,6 +5,8 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"context"
+
 	"github.com/booscaaa/go-api-test-unico/adapter/http/rest"
 	"github.com/booscaaa/go-api-test-unico/adapter/postgres"
 	"github.com/spf13/cobra"
@@ -16,9 +18,11 @@ var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Initialize the REST api",
 	Run: func(cmd *cobra.Command, args []string) {
+		ctx := context.Background()
 		postgres.RunMigrations()
+		database := postgres.GetConnection(ctx)
 		configHTTPServerPort := viper.GetString("server.http.port")
-		rest.InitRest(configHTTPServerPort)
+		rest.InitRest(configHTTPServerPort, database)
 	},
 }
 
--- adapter/http/rest/rest.go
+++ adapter/http/rest/rest.go
@@ -1,11 +1,21 @@
 package rest
 
 import (
+	"github.com/booscaaa/go-api-test-unico/di"
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
 )
 
-func InitRest(httpPort string) {
+func InitRest(httpPort string, database *sqlx.DB) {
+	freeMarketService := di.ConfigFreeMarketDi(database)
+
 	router := gin.Default()
+
+	router.POST("/free-market", freeMarketService.Create)
+	router.GET("/free-market", freeMarketService.Fetch)
+	router.GET("/free-market/:id", freeMarketService.GetByID)
+	router.PUT("/free-market/:id", freeMarketService.Update)
+	router.DELETE("/free-market/:id", freeMarketService.Delete)
 
 	router.Run(":" + httpPort)
 }
